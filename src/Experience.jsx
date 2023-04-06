46d3d6b0a79440174874da50339b7e9848934566
Giovanni Podda <giovanni.podda@icof.it>
2023-04-06 18:47:37 +0200

    first commit

---
+++ src/Experience.jsx
@@ -0,0 +1,35 @@
+import {ContactShadows,PresentationControls,Float,Environment,useGLTF, OrbitControls } from '@react-three/drei'
+
+export default function Experience()
+{
+    const computer = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf')
+
+    return <>
+        <Environment preset='city'/>
+        
+        <color args={[ '#241a1a' ]} attach="background"/>
+        <PresentationControls
+         global 
+         rotation={ [0.13, 0.1,0] }
+         polar={ [-0.4, 0.2] }
+         azimuth={ [-1, 0.75] }
+         config={ {mass:2, tension:400} }
+         snap ={ {mass:2, tension:400} }
+         >
+            <Float rotationIntensity={0.4} >
+                <primitive 
+                    object={ computer.scene } 
+                    position-y={ -1.2 } 
+                >
+                </primitive>
+            </Float>
+        </PresentationControls>
+        <ContactShadows 
+            position-y={-1.4}
+            opacity={0.4}
+            scale={5}
+            blur={2.4}     
+        />
+
+    </>
+}
